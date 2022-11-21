999ec88d079d1d5d1776bb6c4e33b6cd24d918dd
user <user@DESKTOP-U59IHVT>
2022-11-21 10:53:43 +0900

    팀원 커밋!

--- masil-test/src/main/java/main.java
+++ masil-test/src/main/java/main.java
@@ -4,7 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("팀장 커밋!");
-		System.out.println("예슬");
+		System.out.println("팀원 커밋!");
 
 	}
 
